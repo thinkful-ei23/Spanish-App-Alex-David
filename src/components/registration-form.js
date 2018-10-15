4e32c8397e34f18841c4fc384c1eec5b2d8087d2
Alex Puhl <apuhlswimmer@hotmail.com>
2018-10-15 13:59:35 -0600

    removed last name in register, changed first name to just name

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -23,10 +23,8 @@ export class RegistrationForm extends React.Component {
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)
                 )}>
-                <label htmlFor="firstName">First name</label>
-                <Field component={Input} type="text" name="firstName" />
-                <label htmlFor="lastName">Last name</label>
-                <Field component={Input} type="text" name="lastName" />
+                <label htmlFor="name">Name</label>
+                <Field component={Input} type="text" name="name" />
                 <label htmlFor="username">Username</label>
                 <Field
                     component={Input}
